f00ee8ab3f281b74b9443bb5adb0cc17fa0ee764
rustytrooper <diansolo40@gmail.com>
2023-08-05 23:20:46 +0300

    refactor: done task and checked for passing tests

--- src/index.js
+++ src/index.js
@@ -39,6 +39,7 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let morse = Object.entries(MORSE_TABLE)
     let arrFromExpr = Array.from(expr)
     console.log(arrFromExpr);
     let matrix=[]
@@ -46,23 +47,44 @@ function decode(expr) {
     for (let i=0; i<arrFromExpr.length; i+=arrSize) {
       matrix.push(arrFromExpr.slice(i,i+arrSize))
     }
-    console.log(matrix);
-  
-    let slicedStr
   
     let joined = matrix.map(el=>{
      return el.join('')
     })
   console.log(joined);
 
-  let a = joined.map(el=>{
+  let arrWithNumber = joined.map(el=>{
   if(el.includes(1)) {
     return Number(el)
   }
   return el
 })
-  
-  
+let arrWithWhitespace = arrWithNumber.map(el=>{
+  if (el == '**********') {
+    return el.replaceAll(el, ' ')
+  }
+  return String(el)
+})
+
+let dash = arrWithWhitespace.map(el=>{
+  return el.replaceAll('11','-')
+})
+
+let dot= dash.map(el=> {
+  return el.replaceAll('10','.')
+})
+
+let output = dot.map(el=>{
+ for (let i in morse) {
+  if(el==morse[i][0]) {
+    return el.replaceAll(el, morse[i][1])
+  }else if(el==' ') {
+    return el
+  }
+ }
+})
+let endOutput = output.join('')
+return endOutput
 }
 
 module.exports = {
