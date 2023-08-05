eb6dd50f874d99bbbe44733d6df833182ea41aa9
rustytrooper <diansolo40@gmail.com>
2023-08-05 21:19:39 +0300

    init: start doing task, get an array with strings from paramether

--- src/index.js
+++ src/index.js
@@ -39,6 +39,34 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let arrFromExpr = Array.from(expr)
+    console.log(arrFromExpr);
+    let matrix=[]
+    let arrSize=10
+    for (let i=0; i<arrFromExpr.length; i+=arrSize) {
+      matrix.push(arrFromExpr.slice(i,i+arrSize))
+    }
+    console.log(matrix);
+  
+    let slicedStr
+  
+    let joined = matrix.map(el=>{
+     return el.join('')
+    })
+  console.log(joined);
+  
+    joined.map(el=>{
+      console.log(el);
+          for (let i in el) {
+            // console.log(el[i]);
+            if(el[i]==1){
+              // console.log(el);
+              slicedStr=el.slice(i, el.length)
+              
+            }
+          }
+    })
+  
 }
 
 module.exports = {
